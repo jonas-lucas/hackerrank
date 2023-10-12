fcc8f47011ddbb45972ca36f7412d8b7baf67dbb
Jonas <133601253+jonas-lucas@users.noreply.github.com>
2023-10-12 12:17:22 +0000

    forgot this

---
+++ python/medium/write_function.py
@@ -0,0 +1,12 @@
+# Write a Function
+
+def is_leap(year):
+    leap = False
+    
+    if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
